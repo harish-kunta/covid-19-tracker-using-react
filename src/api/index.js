0c4f76f2af6fa7c1ec37c7129efdd67c8624978b
HarishKunta <33089746+harishkunta@users.noreply.github.com>
2020-10-22 23:42:27 -0400

    completed cards

--- src/api/index.js
+++ src/api/index.js
@@ -5,15 +5,14 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
     try{
         const { data : { confirmed , recovered , deaths , lastUpdate} } = await axios.get(url);
+    
         
-        const modifiedData = {
+        return {
             confirmed,
             recovered,
             deaths,
-            lastUpdate,
-        }
-        
-        return modifiedData;
+            lastUpdate
+        };
     }
     catch(error){
 
